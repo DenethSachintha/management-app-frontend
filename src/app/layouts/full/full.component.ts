2fc0837b16b06ea079b7e678fa05c7cdff6e088d
Sachin <kbdsjayathilaka@gmail.com>
2025-03-09 01:15:34 +0530

    SCSS, Nav bar

--- src/app/layouts/full/full.component.ts
+++ src/app/layouts/full/full.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {TopStripComponent} from './top-strip/top-strip.component';
+import {MaterialModule} from '../../material.module';
 
 @Component({
   selector: 'app-full',
   standalone: true,
   imports: [
     RouterOutlet,
-    TopStripComponent
+    TopStripComponent,
+    MaterialModule,
   ],
   templateUrl: './full.component.html',
   styleUrl: './full.component.scss'
--- src/app/layouts/full/top-strip/top-strip.component.ts
+++ src/app/layouts/full/top-strip/top-strip.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {TablerIconsModule} from 'angular-tabler-icons';
+import {MatMenu, MatMenuItem, MatMenuModule, MatMenuTrigger} from '@angular/material/menu';
+import {MatAnchor, MatButton, MatButtonModule} from '@angular/material/button';
+import {MaterialModule} from '../../../material.module';
 
 @Component({
   selector: 'app-top-strip',
   standalone: true,
-  imports: [],
+  imports: [
+    MaterialModule,
+    MatButtonModule,
+    MatMenuModule,
+    TablerIconsModule,
+  ],
   templateUrl: './top-strip.component.html',
   styleUrl: './top-strip.component.scss'
 })
